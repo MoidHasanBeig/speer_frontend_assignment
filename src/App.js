9689c5afdf5f02323704e55edd69308c6905e90b
MoidHasanBeig <MoidHasanBeig@github.com>
2021-07-24 12:34:31 -0400

    Animate menu; Initialize viewport check functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import Menu from './components/Menu/Menu';
 import './App.scss';
 
 function App() {
+  const [activeSection,setActiveSection] = useState("top");
+
+  //scrollThrottler, checkActiveSection, isInViewport together checks the current
+  //section of page which is in view to change various formattings of menu etc.
+  
+  var scrollTimeout;
+  function scrollThrottler() {
+    if ( !scrollTimeout ) {
+      scrollTimeout = setTimeout(function() {
+        scrollTimeout = null;
+        checkActiveSection();
+       }, 66);
+    }
+  }
+
+  function checkActiveSection() {
+    let sectionList = document.getElementsByClassName("section");
+    let sectionArr = Array.from(sectionList);
+    sectionArr.forEach( (section) => {
+      let activeId = isInViewport(section);
+      if (activeId && activeId!==activeSection) {
+        setActiveSection(activeId);
+      }
+    });
+  }
+
+  let isInViewport = function (elem) {
+      let bounding = elem.getBoundingClientRect();
+      let screenHeight = window.innerHeight || document.documentElement.clientHeight;
+      if ( bounding.top > -300 && bounding.top*2 <= (screenHeight)) {
+        return elem.id;
+      }
+      else {
+        return false;
+      }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollThrottler, false);
+    return function cleanUp() {
+      window.removeEventListener("scroll", scrollThrottler, false);
+    }
+  });
+
   return (
     <div className="App">
       <Menu />
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.scss';
 
 const Menu = () => {
+  const [isOpen,setIsOpen] = useState(false);
+
+  function onClick() {
+    setIsOpen( prevValue => !prevValue);
+  }
+
   return (
     <div className="menu-container">
       <div className="logo-container">
-        <div className="hamburger-icon" />
+        <div onClick={onClick} className="hamburger-icon" />
         <div className="brand-name">EXP|CON</div>
       </div>
+      <div className={`${isOpen && 'show-circular-backdrop'} circular-backdrop`} />
+      <ul className={`${isOpen && 'expand-menu'} menu-options`}>
+        <li style={{transitionDelay: "0s"}} className={`${isOpen && 'show-option'} option`}>WHAT IS IT</li>
+        <li style={{transitionDelay: "0.1s"}} className={`${isOpen && 'show-option'} option`}>PERKS</li>
+        <li style={{transitionDelay: "0.2s"}} className={`${isOpen && 'show-option'} option`}>PRICING</li>
+      </ul>
     </div>
   );
 }
