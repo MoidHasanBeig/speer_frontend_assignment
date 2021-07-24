16207d9c96f90eaeec93f4ff0782f5e32f60c27c
MoidHasanBeig <MoidHasanBeig@github.com>
2021-07-23 23:52:57 -0400

    Add and format brand logo and menu btn

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Menu.scss';
+
+const Menu = () => {
+  return (
+    <div className="menu-container">
+      <div className="logo-container">
+        <div className="hamburger-icon" />
+        <div className="brand-name">EXP|CON</div>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
